22ef07a9f68edbcf255e7aefc7029887842d12b1
andreacorallo80-sys <andrea.corallo.80@gmail.com>
2026-03-03 00:31:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import streamlit as st
 import pandas as pd
 import os
 import re
-import PyPDF2
+import pypdf   # <--- ECCOLO! Assicurati che ci sia questo e NON import PyPDF2
 import io
 from fpdf import FPDF
 from datetime import datetime
@@ -424,4 +423,5 @@ if check_password():
                 pdf.output("preventivo_multiplo.pdf")
                 with open("preventivo_multiplo.pdf", "rb") as f:
                     st.download_button("📩 SCARICA IL PREVENTIVO CONGIUNTO", f, f"Offerta_Multipla.pdf", key="dl_multi")
+
 
